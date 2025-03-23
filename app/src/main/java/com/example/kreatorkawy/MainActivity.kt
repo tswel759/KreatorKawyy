8568b5a125d1b929dc03bbfeff2e1e8f38ea9610
pc <szymon.radomski@cyfroweszkoly.pl>
2025-03-23 18:09:22 +0100

    zmienienie sie obrazkow

--- app/src/main/java/com/example/kreatorkawy/MainActivity.kt
+++ app/src/main/java/com/example/kreatorkawy/MainActivity.kt
@@ -5,16 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+import android.widget.RadioGroup
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val obrazekKawy = findViewById<ImageView>(R.id.coffeeImage)
+        val mapaObrazkow = mapOf(
+            R.id.radiobutton_Espresso to R.drawable.espresso,
+            R.id.radiobutton_Capuccino to R.drawable.capuccino,
+            R.id.radiobutton_Latte to R.drawable.latte
+        )
+
+        findViewById<RadioGroup>(R.id.coffeeTypeGroup).setOnCheckedChangeListener { _, wybraneId ->
+            obrazekKawy.setImageResource(mapaObrazkow[wybraneId] ?: R.drawable.espresso)
+        }
         }
     }
-}
